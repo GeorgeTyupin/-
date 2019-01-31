5c1309cccb8b9cff597205d5fe5525f38de933c0
GeorgeTyupin <44722304+GeorgeTyupin@users.noreply.github.com>
2019-01-31 07:52:42 +0300

    Add files via upload

---
+++ ветвь 5/Моя страна Моя семья.js
@@ -0,0 +1,10 @@
+function change_one()  {
+      document.getElementById("img1").src="liniya Lyubvi Petrovny Dolbinoy.JPG" ; 
+     document.getElementById("change").innerHTML = "Линия Любви Петровны Долбиной"  ;
+	}
+		function change_two(id){
+       /* document.getElementsByClassName("active").classList.remove("active");
+        document.getElementsByClassName(id).[0].classList.add("active");  */
+        document.getElementById("img1").src="liniya Yekateriny Semonovnoy Voronkovoy.JPG" ;
+         document.getElementById("change").innerHTML = "Линия Екатерина Семоновной Воронковой "  ;    
+        }
